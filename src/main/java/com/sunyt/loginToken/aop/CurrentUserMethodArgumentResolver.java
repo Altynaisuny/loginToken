3c7632bfcecda9639fd6acd191eb4e439056e2d4
孙远通 <850106619@qq.com>
2019-08-15 07:58:16 +0800

    当前用户注解

---
+++ src/main/java/com/sunyt/loginToken/aop/CurrentUserMethodArgumentResolver.java
@@ -0,0 +1,26 @@
+package com.sunyt.loginToken.aop;
+
+import com.sunyt.loginToken.dto.User;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class CurrentUserMethodArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        if (methodParameter.getParameterType().isAssignableFrom(User.class) &&
+                methodParameter.hasParameterAnnotation(CurrentUser.class)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        //sessionAttribute中取出鉴权时存入的登录用户Id
+        //取出并返回对象。
+        return null;
+    }
+}
